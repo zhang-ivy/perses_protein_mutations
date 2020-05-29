e501edefab97b4f7d6a5881840aabc547966ea56
Ivy Zhang <ivy.zhang@wustl.edu>
2020-05-29 19:01:31 -0400

    generate htfs separately

---
+++ code/generate_htf.py
@@ -0,0 +1,14 @@
+import itertools
+import pickle
+from perses.app.relative_point_mutation_setup import PointMutationExecutor
+
+amino_acids = ['ALA', 'CYS', 'SER', 'THR']
+for wt, proposed in itertools.permutations(amino_acids,r=2):
+     # Create hybrid topology factory
+     apm_delivery = PointMutationExecutor(f"../input/{wt.lower()}_vacuum.pdb", 
+                              '1', 
+                              '2', 
+                              proposed,
+                             )
+     apo_htf = apm_delivery.get_apo_htf()
+     pickle.dump(apo_htf, open(f"../data/{wt}_{proposed}_solvent.pickle", "wb" ))
