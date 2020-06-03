329092a83687811a40c64552e2e1df69d1ab406a
Ivy Zhang <ivy.zhang@wustl.edu>
2020-06-03 17:30:11 -0400

    update generate htf

--- code/generate_htf.py
+++ code/generate_htf.py
@@ -1,19 +1,26 @@
+import argparse
 import itertools
 import pickle
 from perses.app.relative_point_mutation_setup import PointMutationExecutor
 import simtk.openmm.app as app
 
+# Read WT and proposed residue abbreviations
+parser = argparse.ArgumentParser(description='generate htfs')
+parser.add_argument('output_dir', type=str, help='path to output htf')
+args = parser.parse_args()
+
+
 amino_acids = ['ALA', 'CYS', 'SER', 'THR']
-# # Solvent
-# for wt, proposed in itertools.permutations(amino_acids,r=2):
-#      # Create hybrid topology factory
-#      apm_delivery = PointMutationExecutor(f"../input/{wt.lower()}_vacuum.pdb", 
-#                               '1', 
-#                               '2', 
-#                               proposed,
-#                              )
-#      apo_htf = apm_delivery.get_apo_htf()
-#      pickle.dump(apo_htf, open(f"../data/solvent/{wt}_{proposed}_solvent.pickle", "wb" ))
+# Solvent
+for wt, proposed in itertools.permutations(amino_acids,r=2):
+     # Create hybrid topology factory
+     apm_delivery = PointMutationExecutor(f"../input/{wt.lower()}_vacuum.pdb", 
+                              '1', 
+                              '2', 
+                              proposed,
+                             )
+     apo_htf = apm_delivery.get_apo_htf()
+     pickle.dump(apo_htf, open(os.path.join(args.output_dir, f"solvent/{wt}_{proposed}_solvent.pickle"), "wb" ))
 
 # Vacuum
 for wt, proposed in itertools.permutations(amino_acids,r=2):
@@ -28,4 +35,4 @@ for wt, proposed in itertools.permutations(amino_acids,r=2):
 	                          nonperiodic_forcefield_kwargs={'nonbondedMethod': app.NoCutoff}
                              )
      apo_htf = apm_delivery.get_apo_htf()
-     pickle.dump(apo_htf, open(f"../data/vacuum/{wt}_{proposed}_vacuum.pickle", "wb" ))
+     pickle.dump(apo_htf, open(os.path.join(args.output_dir, f"vacuum/{wt}_{proposed}_vacuum.pickle"), "wb" ))
