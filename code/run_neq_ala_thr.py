20c56ab512ad0df324ae29d29107f460e3546d51
Ivy Zhang <ivy.zhang@wustl.edu>
2020-07-02 13:43:44 -0400

    edit ala thr to use 1 cycle and save/log less

--- code/run_neq_ala_thr.py
+++ code/run_neq_ala_thr.py
@@ -66,16 +66,22 @@ context.setPositions(positions)
 openmm.LocalEnergyMinimizer.minimize(context)
 
 # Run neq
-ncycles = 10
+ncycles = 1
 forward_works_master, reverse_works_master = list(), list()
 forward_equil, forward_neq_old, forward_neq_new, reverse_equil, reverse_neq_old, reverse_neq_new = list(), list(), list(), list(), list(), list()
+
 for cycle in range(ncycles):
     # Equilibrium (lambda = 0)
-    _logger.info(f'Cycle: {cycle}, Starting to equilibrate at lambda = 0')
+    initial_time = time.time()
     for step in range(nsteps_eq):
         if step % 750 == 0:
+            elapsed_time = (time.time() - initial_time) * unit.seconds
             forward_equil = forward_equil[-10:]
-        initial_time = time.time()
+            pos = context.getState(getPositions=True, enforcePeriodicBox=False).getPositions(asNumpy=True)
+            old_pos = np.asarray(htf.old_positions(pos))
+            forward_equil.append(old_pos)
+            _logger.info(f'Cycle: {cycle}, forward equil step: {step}, took: {elapsed_time / unit.seconds} seconds')
+            initial_time = time.time()
         try:
             integrator.step(1)
         except:
@@ -83,19 +89,22 @@ for cycle in range(ncycles):
             with open(os.path.join(args.dir, f"{i}_{args.phase}_forward_equil_pos.npy"), 'wb') as f: # change filenames when distributing jobs
                  np.save(f, forward_equil)
             sys.exit()
-        elapsed_time = (time.time() - initial_time) * unit.seconds
-        pos = context.getState(getPositions=True, enforcePeriodicBox=False).getPositions(asNumpy=True)
-        old_pos = np.asarray(htf.old_positions(pos))
-        forward_equil.append(old_pos)
-        _logger.info(f'Cycle: {cycle}, forward equil step: {step}, took: {elapsed_time / unit.seconds} seconds')
 
     # Forward (0 -> 1)
     forward_works = [integrator.get_protocol_work(dimensionless=True)]
+    initial_time = time.time()
     for fwd_step in range(nsteps_neq):
-        initial_time = time.time()
         if fwd_step % 750 == 0:
             forward_neq_old = forward_neq_old[-10:]
             forward_neq_new = forward_neq_new[-10:]
+            elapsed_time = (time.time() - initial_time) * unit.seconds
+            _logger.info(f'Cycle: {cycle}, forward NEQ step: {fwd_step}, took: {elapsed_time / unit.seconds} seconds')
+            pos = context.getState(getPositions=True, enforcePeriodicBox=False).getPositions(asNumpy=True)
+            old_pos = np.asarray(htf.old_positions(pos))
+            forward_neq_old.append(old_pos)
+            new_pos = np.asarray(htf.new_positions(pos))
+            forward_neq_new.append(new_pos)
+            initial_time = time.time()
         try:
             integrator.step(1)
         except:
@@ -105,23 +114,21 @@ for cycle in range(ncycles):
             with open(os.path.join(args.dir, f"{i}_{args.phase}_forward_neq_new_pos.npy"), 'wb') as f: # change filenames when distributing jobs
                 np.save(f, forward_neq_new)
             sys.exit()
-        elapsed_time = (time.time() - initial_time) * unit.seconds
-        _logger.info(f'Cycle: {cycle}, forward NEQ step: {fwd_step}, took: {elapsed_time / unit.seconds} seconds')
-        pos = context.getState(getPositions=True, enforcePeriodicBox=False).getPositions(asNumpy=True)
-        old_pos = np.asarray(htf.old_positions(pos))
-        forward_neq_old.append(old_pos)
-        new_pos = np.asarray(htf.new_positions(pos))
-        forward_neq_new.append(new_pos)
         forward_works.append(integrator.get_protocol_work(dimensionless=True))
     forward_works_master.append(forward_works)
 
 
     # Equilibrium (lambda = 1)
-    _logger.info(f'Cycle: {cycle}, Starting to equilibrate at lambda = 1')
+    initial_time = time.time()
     for step in range(nsteps_eq):
         if step % 750 == 0:
+            elapsed_time = (time.time() - initial_time) * unit.seconds
             reverse_equil = reverse_equil[-10:]
-        initial_time = time.time()
+            pos = context.getState(getPositions=True, enforcePeriodicBox=False).getPositions(asNumpy=True)
+            old_pos = np.asarray(htf.old_positions(pos))
+            reverse_equil.append(old_pos)
+            _logger.info(f'Cycle: {cycle}, reverse equil step: {step}, took: {elapsed_time / unit.seconds} seconds')
+            initial_time = time.time()
         try:
             integrator.step(1)
         except:
@@ -129,19 +136,22 @@ for cycle in range(ncycles):
             with open(os.path.join(args.dir, f"{i}_{args.phase}_reverse_equil_pos.npy"), 'wb') as f: # change filenames when distributing jobs
                  np.save(f, reverse_equil)
             sys.exit()
-        elapsed_time = (time.time() - initial_time) * unit.seconds
-        pos = context.getState(getPositions=True, enforcePeriodicBox=False).getPositions(asNumpy=True)
-        new_pos = np.asarray(htf.new_positions(pos))
-        reverse_equil.append(new_pos)
-        _logger.info(f'Cycle: {cycle}, forward equil step: {step}, took: {elapsed_time / unit.seconds} seconds')
 
-    # Reverse work (1 -> 0)
+    # Reverse (0 -> 1)
     reverse_works = [integrator.get_protocol_work(dimensionless=True)]
+    initial_time = time.time()
     for rev_step in range(nsteps_neq):
-        initial_time = time.time()
         if rev_step % 750 == 0:
             reverse_neq_old = reverse_neq_old[-10:]
             reverse_neq_new = reverse_neq_new[-10:]
+            elapsed_time = (time.time() - initial_time) * unit.seconds
+            _logger.info(f'Cycle: {cycle}, reverse NEQ step: {rev_step}, took: {elapsed_time / unit.seconds} seconds')
+            pos = context.getState(getPositions=True, enforcePeriodicBox=False).getPositions(asNumpy=True)
+            old_pos = np.asarray(htf.old_positions(pos))
+            reverse_neq_old.append(old_pos)
+            new_pos = np.asarray(htf.new_positions(pos))
+            reverse_neq_new.append(new_pos)
+            initial_time = time.time()
         try:
             integrator.step(1)
         except:
@@ -151,13 +161,6 @@ for cycle in range(ncycles):
             with open(os.path.join(args.dir, f"{i}_{args.phase}_reverse_neq_new_pos.npy"), 'wb') as f: # change filenames when distributing jobs
                 np.save(f, reverse_neq_new)
             sys.exit()
-        elapsed_time = (time.time() - initial_time) * unit.seconds
-        _logger.info(f'Cycle: {cycle}, forward NEQ step: {rev_step}, took: {elapsed_time / unit.seconds} seconds')
-        pos = context.getState(getPositions=True, enforcePeriodicBox=False).getPositions(asNumpy=True)
-        old_pos = np.asarray(htf.old_positions(pos))
-        reverse_neq_old.append(old_pos)
-        new_pos = np.asarray(htf.new_positions(pos))
-        reverse_neq_new.append(new_pos)
         reverse_works.append(integrator.get_protocol_work(dimensionless=True))
     reverse_works_master.append(reverse_works)
 
