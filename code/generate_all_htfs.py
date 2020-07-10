ee093e459b0be4e69060f78fd25ba4eaa1aa6750
Ivy Zhang <ivy.zhang@wustl.edu>
2020-07-10 16:46:48 -0400

    fix spacing

--- code/generate_all_htfs.py
+++ code/generate_all_htfs.py
@@ -13,18 +13,18 @@ outdir = "/data/chodera/zhangi/perses_benchmark/test_maps/"
 for wt in amino_acids[1:]:
     if wt != 'ALA':
     for proposed in amino_acids:
-	    if wt != proposed:
-	        print(f"vacuum: {wt}, {proposed}")
-	        # Create hybrid topology factory
-	        apo_delivery = PointMutationExecutor(os.path.join(outdir, f"{wt.lower()}_vacuum.pdb"), 
-	                                  '1', 
-	                                  '2', 
-	                                  proposed,
-	                                  phase='vacuum',
-	                              barostat=None,
-	                              periodic_forcefield_kwargs=None, 
-	                              nonperiodic_forcefield_kwargs={'nonbondedMethod': app.NoCutoff}
-	                                 )
-	        pickle.dump(apo_delivery.get_apo_htf(), open(os.path.join(outdir, f"{wt.lower()}_{proposed.lower()}_vacuum.pickle"), "wb" ))
+        if wt != proposed:
+            print(f"vacuum: {wt}, {proposed}")
+            # Create hybrid topology factory
+            apo_delivery = PointMutationExecutor(os.path.join(outdir, f"{wt.lower()}_vacuum.pdb"), 
+                                      '1', 
+                                      '2', 
+                                      proposed,
+                                      phase='vacuum',
+                                  barostat=None,
+                                  periodic_forcefield_kwargs=None, 
+                                  nonperiodic_forcefield_kwargs={'nonbondedMethod': app.NoCutoff}
+                                     )
+            pickle.dump(apo_delivery.get_apo_htf(), open(os.path.join(outdir, f"{wt.lower()}_{proposed.lower()}_vacuum.pickle"), "wb" ))
 
 
