6ddd0485f18054868b97a970a34cf355c019702a
Ivy Zhang <ivy.zhang@wustl.edu>
2020-08-21 15:11:49 -0400

    change i to dir_num

--- code/generate_t_repex_cache_2fs_restraints.py
+++ code/generate_t_repex_cache_2fs_restraints.py
@@ -19,7 +19,7 @@ parser.add_argument('state', type=int, help='aka lambda, e.g. 0 or 1')
 parser.add_argument('length', type=int, help='in ns')
 args = parser.parse_args()
 
-i = os.path.basename(os.path.dirname(args.dir))
+dir_num = os.path.basename(os.path.dirname(args.dir))
 htf = pickle.load(open(os.path.join(args.dir, f"{i}_{args.phase}.pickle"), "rb" ))
 
 # # Add RMSD force
@@ -43,8 +43,8 @@ print(htf.hybrid_system.getForce(5).getTorsionParameters(47))
 n_replicas = 12  # Number of temperature replicas.
 T_min = 298.0 * unit.kelvin  # Minimum temperature.
 T_max = 600.0 * unit.kelvin  # Maximum temperature.
-temperatures = [T_min + (T_max - T_min) * (math.exp(float(i) / float(n_replicas-1)) - 1.0) / (math.e - 1.0)
-                for i in range(n_replicas)]
+temperatures = [T_min + (T_max - T_min) * (math.exp(float(j) / float(n_replicas-1)) - 1.0) / (math.e - 1.0)
+                for j in range(n_replicas)]
 
 alchemical_state = RelativeAlchemicalState.from_system(htf.hybrid_system)
 alchemical_state.set_alchemical_parameters(args.state)
@@ -59,7 +59,7 @@ move = mcmc.GHMCMove(timestep=2.0*unit.femtoseconds, n_steps=500)
 simulation = multistate.ReplicaExchangeSampler(mcmc_moves=move, number_of_iterations=args.length*1000)
 
 # Run t-repex
-reporter_file = os.path.join(args.dir, f"{i}_{args.phase}_{args.name.lower()}_{args.length}ns.nc")
+reporter_file = os.path.join(args.dir, f"{dir_num}_{args.phase}_{args.name.lower()}_{args.length}ns.nc")
 print(reporter_file)
 reporter = multistate.MultiStateReporter(reporter_file, checkpoint_interval=1)
 simulation.create(thermodynamic_states=compound_states,
