1359328f0df1dfe829ef3c77afee2c657b2fa154
Ivy Zhang <ivy.zhang@wustl.edu>
2020-08-24 13:33:57 -0400

    allow length arg

--- code/restart_t_repex_cache_2fs_restraints.py
+++ code/restart_t_repex_cache_2fs_restraints.py
@@ -5,10 +5,11 @@ import os
 # Read args
 parser = argparse.ArgumentParser(description='run t-repex')
 parser.add_argument('dir', type=str, help='path to input/output dir')
+parser.add_argument('length', type=int, help='in ns')
 args = parser.parse_args()
 
 # Set up sampler
 simulation = ReplicaExchangeSampler.from_storage(os.path.join(args.dir, "18_vacuum_thr_5ns.nc"))
 
 # Run t-repex
-simulation.extend(n_iterations=5000)
+simulation.extend(n_iterations=args.length*1000)
