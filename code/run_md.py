003a42a9325c000dbab56b23545cee83b8916e55
Ivy Zhang <ivy.zhang@wustl.edu>
2020-09-03 13:02:09 -0400

    remove splitting

--- code/run_md.py
+++ code/run_md.py
@@ -38,7 +38,7 @@ with open(os.path.join(args.dir, f"{dir_num}_{args.phase}.pickle"), 'rb') as f:
 equilibrated_pdb_filename = f'{args.phase}_equilibrated.pdb'
 
 # Make integrator
-integrator = LangevinIntegrator(temperature, collision_rate, timestep, splitting=splitting)
+integrator = LangevinIntegrator(temperature, collision_rate, timestep)
 
 # Minimize
 print('Minimizing energy...')
