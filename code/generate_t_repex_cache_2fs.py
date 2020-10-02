58a965ff45c5e42257ecd8d3361e39d4fc279e50
Ivy Zhang <ivy.zhang@wustl.edu>
2020-10-02 00:30:00 -0400

    add flatten torsions

--- code/generate_t_repex_cache_2fs.py
+++ code/generate_t_repex_cache_2fs.py
@@ -17,10 +17,57 @@ parser.add_argument('phase', type=str, help='solvent or vacuum')
 parser.add_argument('name', type=str, help='amino acid three letter code, e.g. ALA')
 parser.add_argument('state', type=int, help='aka lambda, e.g. 0 or 1')
 parser.add_argument('length', type=int, help='in ns')
+parse.add_argument('direction' type=str, help="forward or reverse")
 args = parser.parse_args()
 
 i = os.path.basename(os.path.dirname(args.dir))
-htf = pickle.load(open(os.path.join(args.dir, f"{i}_{args.phase}.pickle"), "rb" ))
+
+if args.direction == "forward":
+	# Generate htf for capped ALA->THR in vacuum
+	atp, sys_gen = generate_atp()
+
+	# At alanine endstate
+	htf = generate_dipeptide_top_pos_sys(atp.topology, 
+	                                         new_res = 'THR', 
+	                                         system = atp.system, 
+	                                         positions = atp.positions,
+	                                         system_generator = sys_gen, 
+	                                         conduct_htf_prop=True,
+	                                         flatten_torsions=True, 
+	                                         validate_endstate_energy=False)
+elif args.direction == 'reverse':
+
+	# Generate htf for capped THR->ALA in vacuum
+	pdb = app.PDBFile("../input/thr_vacuum.pdb")
+
+	forcefield_files = ['amber14/protein.ff14SB.xml', 'amber14/tip3p.xml']
+	barostat = None
+	system_generator = SystemGenerator(forcefields=forcefield_files,
+	                               barostat=barostat,
+	                               forcefield_kwargs={'removeCMMotion': False,
+	                                                    'ewaldErrorTolerance': 1e-4,
+	                                                    'constraints' : app.HBonds,
+	                                                    'hydrogenMass' : 4 * unit.amus},
+	                                periodic_forcefield_kwargs=None,
+	                                small_molecule_forcefield='gaff-2.11',
+	                                nonperiodic_forcefield_kwargs = {'nonbondedMethod': app.NoCutoff}, 
+	                                   molecules=None, 
+	                                   cache=None)
+	system = system_generator.create_system(pdb.topology) 
+	positions = unit.quantity.Quantity(value = np.array([list(atom_pos) for atom_pos in pdb.positions.value_in_unit_system(unit.md_unit_system)]), unit = unit.nanometers)
+
+	htf = generate_dipeptide_top_pos_sys(pdb.topology, 
+	                                         new_res = 'ALA', 
+	                                         system = system, 
+	                                         positions = positions,
+	                                         system_generator = system_generator, 
+	                                         conduct_htf_prop=True, 
+	                                         flatten_torsions=True, 
+	                                         validate_endstate_energy=False)
+
+# Render atom map
+atom_map_filename = f'{args.dir}/atom_map.png'
+render_protein_residue_atom_mapping(htf._topology_proposal, atom_map_filename)
 
 # Create states for each replica
 n_replicas = 12  # Number of temperature replicas.
