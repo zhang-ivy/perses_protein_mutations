a6911136091b73581ba6616e997154a10493c3f2
Ivy Zhang <ivy.zhang@wustl.edu>
2020-12-07 14:11:18 -0500

    remove direction arg

--- code/13_capped_aa_softened_repex/run_repex_softened_endstates.py
+++ code/13_capped_aa_softened_repex/run_repex_softened_endstates.py
@@ -20,7 +20,6 @@ parser.add_argument('phase', type=str, help='solvent or vacuum')
 parser.add_argument('name', type=str, help='amino acid three letter code, e.g. ALA, corresponding to the endstate')
 parser.add_argument('endstate', type=int, help='aka lambda, e.g. 0 or 1')
 parser.add_argument('length', type=int, help='in ns')
-parser.add_argument('direction', type=str, help="forward or reverse")
 args = parser.parse_args()
 
 # Read htf
