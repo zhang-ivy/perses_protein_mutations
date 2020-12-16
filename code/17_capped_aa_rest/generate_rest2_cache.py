315a2663ef117c81b27bf4b9629e67d936686680
Ivy Zhang <ivy.zhang@wustl.edu>
2020-12-16 10:06:56 -0500

    minimize each sampler state

--- code/17_capped_aa_rest/generate_rest2_cache.py
+++ code/17_capped_aa_rest/generate_rest2_cache.py
@@ -3,6 +3,9 @@ import os
 from perses.annihilation.rest import RESTTopologyFactory
 from perses.annihilation.lambda_protocol import RESTState
 from openmmtools.states import SamplerState, ThermodynamicState, CompoundThermodynamicState
+from openmmtools import cache, utils
+from perses.dispersed.utils import configure_platform
+cache.global_context_cache.platform = configure_platform(utils.get_fastest_platform().getName())
 from simtk import openmm, unit
 import math
 from openmmtools.constants import kB
@@ -41,15 +44,23 @@ lambda_zero_alchemical_state = RESTState.from_system(REST_system)
 thermostate = ThermodynamicState(REST_system, temperature=T_min)
 compound_thermodynamic_state = CompoundThermodynamicState(thermostate, composable_states=[lambda_zero_alchemical_state])
 
+context_cache = cache.ContextCache()
+
 # Create thermodynamics states
 sampler_state =  SamplerState(htf.hybrid_positions, box_vectors=htf.hybrid_system.getDefaultPeriodicBoxVectors())
 beta_0 = 1/(kB*T_min)
 thermodynamic_state_list = []
+sampler_state_list = []
 for temperature in temperatures:
     beta_m = 1/(kB*temperature)
     compound_thermodynamic_state_copy = copy.deepcopy(compound_thermodynamic_state)
     compound_thermodynamic_state_copy.set_alchemical_parameters(beta_0, beta_m)
     thermodynamic_state_list.append(compound_thermodynamic_state_copy)
+
+    # now generating a sampler_state for each thermodyanmic state, with relaxed positions
+    context, context_integrator = context_cache.get_context(compound_thermodynamic_state_copy)
+    feptasks.minimize(compound_thermodynamic_state_copy, sampler_state)
+    sampler_state_list.append(copy.deepcopy(sampler_state))
 
 # Set up sampler
 move = mcmc.GHMCMove(timestep=4.0*unit.femtoseconds, n_steps=250)
@@ -59,6 +70,6 @@ simulation = multistate.ReplicaExchangeSampler(mcmc_moves=move, number_of_iterat
 reporter_file = os.path.join(args.dir, f"{i}_{args.phase}_{args.name.lower()}_{args.length}ns.nc")
 reporter = multistate.MultiStateReporter(reporter_file, checkpoint_interval=1)
 simulation.create(thermodynamic_states=thermodynamic_state_list,
-                  sampler_states=sampler_state,
+                  sampler_states=sampler_state_list,
                   storage=reporter)
 simulation.run()
