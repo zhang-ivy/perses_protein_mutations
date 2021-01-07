2ee5f30ca8e4f496db7ecb67c2818e6eda9e0fb0
Ivy Zhang <ivy.zhang@wustl.edu>
2021-01-07 12:36:19 -0500

    fix conditionals

--- code/18_bnbs_rest/generate_rest2_cache_bnbs_expanded.py
+++ code/18_bnbs_rest/generate_rest2_cache_bnbs_expanded.py
@@ -32,11 +32,11 @@ htf = pickle.load(open(os.path.join(args.dir, f"{i}_{args.phase}_{args.state}.pi
 # Build REST factory
 if args.phase == 'apo' and args.direction == 'backward':
     factory = RESTTopologyFactory(htf.hybrid_system, solute_region=list(range(669, 683)) + list(range(14877, 14881)) + list(range(604, 628)))
-# elif args.phase == 'complex' and args.direction == 'forward':
-#     factory = RESTTopologyFactory(htf.hybrid_system, solute_region=list(range(669, 683)) + list(range(29474, 29478)))
-elif args.phase == 'apo' and args.direction == 'backward':
-    factory = RESTTopologyFactory(htf.hybrid_system, solute_region=list(range(669, 679)) + list(range(14873, 14881)) + list(range(604, 628)))
 # elif args.phase == 'complex' and args.direction == 'backward':
+#     factory = RESTTopologyFactory(htf.hybrid_system, solute_region=list(range(669, 683)) + list(range(29474, 29478)))
+elif args.phase == 'apo' and args.direction == 'forward':
+    factory = RESTTopologyFactory(htf.hybrid_system, solute_region=list(range(669, 679)) + list(range(14873, 14881)) + list(range(604, 628)))
+# elif args.phase == 'complex' and args.direction == 'forward':
 #     factory = RESTTopologyFactory(htf.hybrid_system, solute_region=list(range(669, 679)) + list(range(29470, 29478)))
 
 
