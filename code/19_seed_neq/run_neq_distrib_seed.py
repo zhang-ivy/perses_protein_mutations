c0dcbb3f4dc262d706bcaabd086b7c170e4f4116
Ivy Zhang <ivy.zhang@wustl.edu>
2021-01-19 18:55:02 -0500

    add ala snapshot read in

--- code/19_seed_neq/run_neq_distrib_seed.py
+++ code/19_seed_neq/run_neq_distrib_seed.py
@@ -46,23 +46,23 @@ i = os.path.basename(os.path.dirname(args.dir))
 with open(os.path.join(args.dir, f"{i}_{args.phase}.pickle"), 'rb') as f:
     htf = pickle.load(f)
 
-# Read in lambda = 0 cache, if necessary
+# Read in lambda = 0 cache
 if args.direction == 'forward':
     with open(os.path.join(args.dir, f"{i}_{args.phase}_thr_5ns_snapshots.npy"), 'rb') as f:
         subset_pos = np.load(f)
 
-    system = htf.hybrid_system
     # index = 0 if args.sim_number - 1 < 33 else (1 if args.sim_number -1 < 66 else 2) # 0-32, 33-65, 66-99
     # if args.phase == 'apo':
     #     index = 0 if args.sim_number -1 < 50 else 1
     # elif args.phase == 'complex':
     #     index = 0
 
-    positions = subset_pos[args.sim_number - 1]
 else:
-    # Set system and positions 
-    system = htf.hybrid_system
-    positions = htf.hybrid_positions
+    with open(os.path.join(args.dir, f"{i}_{args.phase}_ala_5ns_snapshots.npy"), 'rb') as f:
+        subset_pos = np.load(f)
+    
+positions = subset_pos[args.sim_number - 1]
+system = htf.hybrid_system
 
 # Set up integrator
 integrator = PeriodicNonequilibriumIntegrator(DEFAULT_ALCHEMICAL_FUNCTIONS, nsteps_eq, nsteps_neq, neq_splitting, timestep=timestep)
@@ -123,8 +123,12 @@ if args.direction == 'backward':
     #     index = 0 if args.sim_number -1 < 50 else 1
     # elif args.phase == 'complex':
     #     index = 0
-    positions = subset_pos[args.sim_number - 1]
-    context.setPositions(positions)
+else:
+    with open(os.path.join(args.dir, f"{i}_{args.phase}_ala_5ns_snapshots.npy"), 'rb') as f:
+        subset_pos = np.load(f)
+
+positions = subset_pos[args.sim_number - 1]
+context.setPositions(positions)
 
 # Run eq reverse (1 -> 0)
 reverse_eq_old, reverse_eq_new = list(), list()
