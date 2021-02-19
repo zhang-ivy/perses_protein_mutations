c4b2aa9fb2a104a62f49777371f38145ab79cd37
Ivy Zhang <ivy.zhang@wustl.edu>
2021-02-19 11:07:26 -0700

    fix for loop

--- code/18_bnbs_rest/prep_rest_jobs.py
+++ code/18_bnbs_rest/prep_rest_jobs.py
@@ -18,38 +18,38 @@ for k, v in dictionary.items():
 	new = k
 	old, old_aa, new_aa, resid = v
 
-# Edit template bash file
-with open(template_file, "r") as f:
-    lines_out = f.readlines()
+	# Edit template bash file
+	with open(template_file, "r") as f:
+	    lines_out = f.readlines()
 
-lines_new = []
-for line in lines_out:
-    if "#BSUB -W" in line:
-        line = line[:9] + str(wall_time) + line[11:]
-    elif "#BSUB -o" in line:
-        line = line[:12] + str(new) + ".%I.out\n"
-    elif "#BSUB -eo" in line:
-        line = line[:13] + str(new) + ".%I.stderr\n"
-    elif "#BSUB -n" in line:
-        line = line[:26] + str(memory) + line[27:] 
-    elif "#BSUB -J" in line:
-        line = line[:13] + str(new) + '[1-4]"\n'
-    elif "old=" in line:
-        line = f"old={old}\n"
-    elif "new=" in line:
-        line = f"new={new}\n"
-    elif "old_aa=" in line:
-        line = f"old_aa={old_aa}\n"
-    elif "new_aa=" in line:
-        line = f"new_aa={new_aa}\n"
-    elif "resid=" in line:
-        line = f"resid={resid}\n"
-    elif "python generate_rest2_cache" in line:
-        line = line[:51] + str(script_version) + line[52:]
-    lines_new.append(line)
+	lines_new = []
+	for line in lines_out:
+	    if "#BSUB -W" in line:
+	        line = line[:9] + str(wall_time) + line[11:]
+	    elif "#BSUB -o" in line:
+	        line = line[:12] + str(new) + ".%I.out\n"
+	    elif "#BSUB -eo" in line:
+	        line = line[:13] + str(new) + ".%I.stderr\n"
+	    elif "#BSUB -n" in line:
+	        line = line[:26] + str(memory) + line[27:] 
+	    elif "#BSUB -J" in line:
+	        line = line[:13] + str(new) + '[1-4]"\n'
+	    elif "old=" in line:
+	        line = f"old={old}\n"
+	    elif "new=" in line:
+	        line = f"new={new}\n"
+	    elif "old_aa=" in line:
+	        line = f"old_aa={old_aa}\n"
+	    elif "new_aa=" in line:
+	        line = f"new_aa={new_aa}\n"
+	    elif "resid=" in line:
+	        line = f"resid={resid}\n"
+	    elif "python generate_rest2_cache" in line:
+	        line = line[:51] + str(script_version) + line[52:]
+	    lines_new.append(line)
 
-# Make dir and save new bash file
-os.system(f"mkdir {os.path.join(out_dir, str(new))}")
+	# Make dir and save new bash file
+	os.system(f"mkdir {os.path.join(out_dir, str(new))}")
 
-with open(os.path.join(out_dir, str(new), "run_rest2_all.sh"), "w") as f:
-     f.writelines(lines_new)
+	with open(os.path.join(out_dir, str(new), "run_rest2_all.sh"), "w") as f:
+	     f.writelines(lines_new)
