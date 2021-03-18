e7b2eb9b1f486bf748c6d0a93253ca93b961dea5
Ivy Zhang <ivy.zhang@wustl.edu>
2021-03-17 20:56:51 -0400

    update another tab

--- code/23_rbd_ace2_neq/prep_neq_jobs.py
+++ code/23_rbd_ace2_neq/prep_neq_jobs.py
@@ -28,31 +28,31 @@ for k, v in dictionary.items():
 
 		lines_new = []
 		for line in lines_out:
-		if "#BSUB -W" in line:
-			line = line[:9] + str(wall_time) + line[10:]
-		elif "#BSUB -o" in line:
-			line = line[:13] + str(new) + f".{phase}.%I.out\n"
-		elif "#BSUB -eo" in line:
-			line = line[:13] + str(new) + f".{phase}.%I.stderr\n"
-		elif "#BSUB -n" in line:
-			line = line[:26] + str(memory) + line[27:] 
-		elif "#BSUB -J" in line:
-			line = line[:13] + str(new) + f'.{phase}.[1-100]"\n'
-		elif "#BSUB -w" in line:
-			line = line[:9] + f'"done({job})"\n'
-		elif "new=" in line:
-			line = f"new={new}\n"
-		elif "old_aa=" in line:
-			line = f"old_aa={old_aa.lower()}\n"
-		elif "new_aa=" in line:
-			line = f"new_aa={new_aa.lower()}\n"
-		elif "phase=" in line:
-			line = f"phase={phase}\n"
-		elif "length=" in line:
-			line = f"length={length}\n"
-		elif "resid=" in line:
-			line = f"resid={resid}\n"
-		lines_new.append(line)
+			if "#BSUB -W" in line:
+				line = line[:9] + str(wall_time) + line[10:]
+			elif "#BSUB -o" in line:
+				line = line[:13] + str(new) + f".{phase}.%I.out\n"
+			elif "#BSUB -eo" in line:
+				line = line[:13] + str(new) + f".{phase}.%I.stderr\n"
+			elif "#BSUB -n" in line:
+				line = line[:26] + str(memory) + line[27:] 
+			elif "#BSUB -J" in line:
+				line = line[:13] + str(new) + f'.{phase}.[1-100]"\n'
+			elif "#BSUB -w" in line:
+				line = line[:9] + f'"done({job})"\n'
+			elif "new=" in line:
+				line = f"new={new}\n"
+			elif "old_aa=" in line:
+				line = f"old_aa={old_aa.lower()}\n"
+			elif "new_aa=" in line:
+				line = f"new_aa={new_aa.lower()}\n"
+			elif "phase=" in line:
+				line = f"phase={phase}\n"
+			elif "length=" in line:
+				line = f"length={length}\n"
+			elif "resid=" in line:
+				line = f"resid={resid}\n"
+			lines_new.append(line)
 
 		# Make dir and save new bash file
 		os.system(f"mkdir {os.path.join(out_dir, str(new))}")
