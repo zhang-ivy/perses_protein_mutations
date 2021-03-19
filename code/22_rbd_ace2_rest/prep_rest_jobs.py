c093955164c62b4b75f790c7248b5d80d4e22da6
Ivy Zhang <ivy.zhang@wustl.edu>
2021-03-19 16:47:09 -0400

    update prep to have two phases

--- code/22_rbd_ace2_rest/prep_rest_jobs.py
+++ code/22_rbd_ace2_rest/prep_rest_jobs.py
@@ -10,44 +10,47 @@ template_file = "/home/zhangi/choderalab/perses_benchmark/perses_protein_mutatio
 out_dir = "/data/chodera/zhangi/perses_benchmark/neq/14/"
 
 # Define job parameters
-wall_time = 30
-memory = 10
+phase =  ['complex', 'apo']
+wall_time = [30, 15]
+memory = [10, 5]
 script_version = 4
 
 for k, v in dictionary.items():
 	new = k
 	old_aa, new_aa, resid, job = v
+	
+	for i, phase in enumerate(phases):
 
-	# Edit template bash file
-	with open(template_file, "r") as f:
-	    lines_out = f.readlines()
+		# Edit template bash file
+		with open(template_file, "r") as f:
+		    lines_out = f.readlines()
 
-	lines_new = []
-	for line in lines_out:
-	    if "#BSUB -W" in line:
-	        line = line[:9] + str(wall_time) + line[11:]
-	    elif "#BSUB -o" in line:
-	        line = line[:12] + str(new) + ".%I.out\n"
-	    elif "#BSUB -eo" in line:
-	        line = line[:13] + str(new) + ".%I.stderr\n"
-	    elif "#BSUB -n" in line:
-	        line = line[:26] + str(memory) + line[28:] 
-	    elif "#BSUB -J" in line:
-	        line = line[:13] + str(new) + '[1-4]"\n'
-	    elif "#BSUB -w" in line:
-            	line = line[:9] + f'"done({job})"\n'
-	    elif "new=" in line:
-	        line = f"new={new}\n"
-	    elif "old_aa=" in line:
-	        line = f"old_aa={old_aa}\n"
-	    elif "new_aa=" in line:
-	        line = f"new_aa={new_aa}\n"
-	    elif "resid=" in line:
-	        line = f"resid={resid}\n"
-	    lines_new.append(line)
+		lines_new = []
+		for line in lines_out:
+		    if "#BSUB -W" in line:
+		        line = line[:9] + str(wall_time[i]) + line[11:]
+		    elif "#BSUB -o" in line:
+		        line = line[:12] + str(new) + ".%I.out\n"
+		    elif "#BSUB -eo" in line:
+		        line = line[:13] + str(new) + ".%I.stderr\n"
+		    elif "#BSUB -n" in line:
+		        line = line[:26] + str(memory[i]) + line[28:] 
+		    elif "#BSUB -J" in line:
+		        line = line[:13] + str(new) + '[1-4]"\n'
+		    elif "#BSUB -w" in line:
+	            	line = line[:9] + f'"done({job})"\n'
+		    elif "new=" in line:
+		        line = f"new={new}\n"
+		    elif "old_aa=" in line:
+		        line = f"old_aa={old_aa}\n"
+		    elif "new_aa=" in line:
+		        line = f"new_aa={new_aa}\n"
+		    elif "resid=" in line:
+		        line = f"resid={resid}\n"
+		    lines_new.append(line)
 
-	# Make dir and save new bash file
-	os.system(f"mkdir {os.path.join(out_dir, str(new))}")
+		# Make dir and save new bash file
+		os.system(f"mkdir {os.path.join(out_dir, str(new))}")
 
-	with open(os.path.join(out_dir, str(new), "run_rest2_all.sh"), "w") as f:
-	     f.writelines(lines_new)
+		with open(os.path.join(out_dir, str(new), f"run_rest2_{phase}.sh"), "w") as f:
+		     f.writelines(lines_new)
