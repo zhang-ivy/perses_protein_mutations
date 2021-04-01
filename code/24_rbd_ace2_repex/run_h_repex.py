0507e68cab8b40ceca96777f785aaec94be9e6c4
Ivy Zhang <ivy.zhang@wustl.edu>
2021-04-01 17:40:16 -0400

    fix logger

--- code/24_rbd_ace2_repex/run_h_repex.py
+++ code/24_rbd_ace2_repex/run_h_repex.py
@@ -10,8 +10,25 @@ import logging
 import datetime
 
 # Set up logger
+class TimeFilter(logging.Filter):
+    def filter(self, record):
+        try:
+            last = self.last
+        except AttributeError:
+            last = record.relativeCreated
+        delta = datetime.datetime.fromtimestamp(record.relativeCreated/1000.0) - datetime.datetime.fromtimestamp(last/1000.0)
+        record.relative = '{0:.2f}'.format(delta.seconds + delta.microseconds/1000000.0)
+        self.last = record.relativeCreated
+        return True
+fmt = logging.Formatter(fmt="%(asctime)s:(%(relative)ss):%(name)s:%(message)s")
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.DEBUG,
+    datefmt='%Y-%m-%d %H:%M:%S')
 _logger = logging.getLogger()
 _logger.setLevel(logging.DEBUG)
+[hndl.addFilter(TimeFilter()) for hndl in _logger.handlers]
+[hndl.setFormatter(fmt) for hndl in _logger.handlers]
 
 # Read args
 parser = argparse.ArgumentParser(description='run perses protein mutation')
@@ -23,8 +40,7 @@ i = os.path.basename(os.path.dirname(args.dir))
 apo_htf = pickle.load(open(os.path.join(args.dir, f"{i}_{args.phase}.pickle"), "rb" ))
 
 # Build the hybrid repex samplers
-suffix = 'run'; selection = 'not water'; checkpoint_interval = 10; n_states = 11; n_cycles = 5000
-lambda_protocol = LambdaProtocol(functions='default')
+suffix = 'run'; selection = 'not water'; checkpoint_interval = 10; n_states = 11; n_cycles = 5000 lambda_protocol = LambdaProtocol(functions='default')
 reporter_file = os.path.join(args.dir, f"{i}_{args.phase}.nc")
 reporter = MultiStateReporter(reporter_file, analysis_particle_indices = apo_htf.hybrid_topology.select(selection), checkpoint_interval = checkpoint_interval)
 hss = HybridRepexSampler(mcmc_moves=mcmc.LangevinSplittingDynamicsMove(timestep= 4.0 * unit.femtoseconds,
