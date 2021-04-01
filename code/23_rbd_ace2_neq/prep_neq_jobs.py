407e0f495bc43e0b9464d76dd1333ba1f4c46c11
Ivy Zhang <ivy.zhang@wustl.edu>
2021-03-31 23:19:45 -0400

    update job wait

--- code/23_rbd_ace2_neq/prep_neq_jobs.py
+++ code/23_rbd_ace2_neq/prep_neq_jobs.py
@@ -2,7 +2,7 @@ import os
 import yaml
 
 # Load in mutation parameters
-stream = open("test.yaml", 'r')
+stream = open("test_wait.yaml", 'r')
 dictionary = yaml.load(stream)
 
 # Define file paths
@@ -10,17 +10,17 @@ template_file = "/home/zhangi/choderalab/perses_benchmark/perses_protein_mutatio
 out_dir = "/data/chodera/zhangi/perses_benchmark/neq/15/"
 
 # Define job parameters
-wall_time = 6 # hours
-memory = 6 # GB
+wall_time = [5, 2] # hours
+memory = [5, 3] # GB
 length = 1 # ns
 phases = ['complex', 'apo']
 
 for k, v in dictionary.items():
 	print(f"prepping dir {k}")
 	new = k
-	old_aa, new_aa, resid, job = v
+	old_aa, new_aa, resid, job_apo, job_complex = v
 
-	for phase in phases:
+	for i, phase in enumerate(phases):
 
 		# Edit template bash file
 		with open(template_file, "r") as f:
@@ -29,16 +29,17 @@ for k, v in dictionary.items():
 		lines_new = []
 		for line in lines_out:
 			if "#BSUB -W" in line:
-				line = line[:9] + str(wall_time) + line[10:]
+				line = line[:9] + str(wall_time[i]) + line[10:]
 			elif "#BSUB -o" in line:
 				line = line[:13] + str(new) + f".{phase}.%I.out\n"
 			elif "#BSUB -eo" in line:
 				line = line[:13] + str(new) + f".{phase}.%I.stderr\n"
 			elif "#BSUB -n" in line:
-				line = line[:26] + str(memory) + line[27:] 
+				line = line[:26] + str(memory[i]) + line[27:] 
 			elif "#BSUB -J" in line:
 				line = line[:13] + str(new) + f'.{phase}.[1-100]"\n'
 			elif "#BSUB -w" in line:
+                                job = job_apo if phase == 'apo' else job_complex
 				line = line[:9] + f'"done({job})"\n'
 			elif "new=" in line:
 				line = f"new={new}\n"
