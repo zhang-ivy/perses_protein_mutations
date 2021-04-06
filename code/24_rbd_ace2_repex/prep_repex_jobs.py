14a652f3d50250a4236a27fed1224c222e931fc6
Ivy Zhang <ivy.zhang@wustl.edu>
2021-04-06 17:57:20 -0400

    add prep repex sh scripts

---
+++ code/24_rbd_ace2_repex/prep_repex_jobs.py
@@ -0,0 +1,45 @@
+import os
+import yaml
+
+# Load in mutation parameters
+stream = open("test.yaml", 'r')
+dictionary = yaml.load(stream)
+
+# Define file paths
+template_file = "/home/zhangi/choderalab/perses_benchmark/perses_protein_mutations/code/24_rbd_ace2_repex/run_repex_parallel.sh"
+out_dir = "/data/chodera/zhangi/perses_benchmark/repex/31/"
+
+# Define job parameters
+wall_time = [24, 12] # hours
+memory = [7, 4] # GB
+phases = ['complex', 'apo']
+
+for k, v in dictionary.items():
+	print(f"prepping dir {k}")
+	new = k
+
+	for i, phase in enumerate(phases):
+
+		# Edit template bash file
+		with open(template_file, "r") as f:
+			lines_out = f.readlines()
+
+		lines_new = []
+		for line in lines_out:
+			if "#BSUB -W" in line:
+				line = line[:9] + str(wall_time[i]) + line[10:]
+			elif "#BSUB -o" in line:
+				line = line[:9] + f" {phase}.%I.out\n"
+			elif "#BSUB -eo" in line:
+				line = line[:9] + f" {phase}.%I.stderr\n"
+			elif "#BSUB -R" in line:
+				line = line[:21] + str(memory[i]) + line[23:] 
+			elif "#BSUB -J" in line:
+				line = line[:10] + str(new) + f'.{phase}"\n'
+			lines_new.append(line)
+
+		# Make dir and save new bash file
+		os.system(f"mkdir {os.path.join(out_dir, str(new))}")
+
+		with open(os.path.join(out_dir, str(new), "0" ,f"run_repex_parallel_{phase}.sh"), "w") as f:
+			f.writelines(lines_new)
