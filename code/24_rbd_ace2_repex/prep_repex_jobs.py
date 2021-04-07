0daab052c73944a69990bd6d0605c4a32cef26b2
Ivy Zhang <ivy.zhang@wustl.edu>
2021-04-07 10:08:08 -0400

    fix mem again

--- code/24_rbd_ace2_repex/prep_repex_jobs.py
+++ code/24_rbd_ace2_repex/prep_repex_jobs.py
@@ -33,7 +33,7 @@ for k, v in dictionary.items():
 			elif "#BSUB -eo" in line:
 				line = line[:9] + f" {phase}.stderr\n"
 			elif "#BSUB -n 1 -R" in line:
-				line = line[:25] + str(memory[i]) + line[25:] 
+				line = line[:26] + str(memory[i]) + line[27:] 
 			elif "#BSUB -J" in line:
 				line = line[:10] + str(new) + f'.{phase}"\n'
 			elif "python " in line:
