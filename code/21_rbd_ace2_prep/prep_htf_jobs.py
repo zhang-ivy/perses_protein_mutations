85534b66748a4c3d6b2e656b955411bd31c9bb78
Ivy Zhang <ivy.zhang@wustl.edu>
2021-04-08 09:54:43 -0400

    update prep htf script

--- code/21_rbd_ace2_prep/prep_htf_jobs.py
+++ code/21_rbd_ace2_prep/prep_htf_jobs.py
@@ -13,31 +13,31 @@ out_dir = "/data/chodera/zhangi/perses_benchmark/repex/31/"
 for k, v in dictionary.items():
 	print(f"prepping dir {k}")
 	new = k
-        old, resid, new_aa = v
+	old, resid, new_aa = v
 
+	# Edit template bash file
+	with open(template_file, "r") as f:
+		lines_out = f.readlines()
 
-		# Edit template bash file
-		with open(template_file, "r") as f:
-			lines_out = f.readlines()
+	lines_new = []
+	for line in lines_out:
+		elif "#BSUB -o" in line:
+			line = line[:12] + f"{new}.out\n"
+		elif "#BSUB -eo" in line:
+			line = line[:13] + f"{new}.stderr\n"
+		elif "#BSUB -J" in line:
+			line = line[:13] + str(new) + '"'
+		elif "old= " in line:
+			line = f"old={old}"
+		elif "new= " in line:
+			line = f"new={new}"
+		elif "resid=" in line:
+			line = f"resid={resid}"
+		elif "new_aa=" in line:
+			line = f"new_aa={new_aa}"
 
-		lines_new = []
-		for line in lines_out:
-			if "#BSUB -W" in line:
-				line = line[:9] + str(wall_time[i]) + line[10:]
-			elif "#BSUB -o" in line:
-				line = line[:9] + f" {phase}.out\n"
-			elif "#BSUB -eo" in line:
-				line = line[:9] + f" {phase}.stderr\n"
-			elif "#BSUB -n 1 -R" in line:
-				line = line[:26] + str(memory[i]) + line[27:] 
-			elif "#BSUB -J" in line:
-				line = line[:10] + str(new) + f'.{phase}"\n'
-			elif "python " in line:
-				line = f"python run_h_repex.py /data/chodera/zhangi/perses_benchmark/repex/31/{new}/0/ {phase}"
-			lines_new.append(line)
+		lines_new.append(line)
 
-		# Make dir and save new bash file
-		os.system(f"mkdir {os.path.join(out_dir, str(new))}")
-
-		with open(os.path.join(out_dir, str(new), "0" ,f"run_repex_{phase}.sh"), "w") as f:
-			f.writelines(lines_new)
+	# Save bash file
+	with open(os.path.join(out_dir, str(new), "0" ,f"generate_htf.sh"), "w") as f:
+		f.writelines(lines_new)
