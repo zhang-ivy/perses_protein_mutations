aa2518b4f9a0a59ecd246439aaf9bbad8dc2798e
Ivy Zhang <ivy.zhang@wustl.edu>
2021-04-19 18:02:38 -0400

    add logger debug

--- code/24_rbd_ace2_repex/run_h_repex_dipeptide.py
+++ code/24_rbd_ace2_repex/run_h_repex_dipeptide.py
@@ -5,12 +5,16 @@ from openmmtools.multistate import ReplicaExchangeSampler
 testsystem = testsystems.AlanineDipeptideExplicit()
 import os
 import tempfile
+import logging
 
 n_replicas = 11  # Number of temperature replicas.
 T_min = 300.0 * unit.kelvin  # Minimum temperature.
 T_max = 600.0 * unit.kelvin  # Maximum temperature.
 temperatures = [T_min + (T_max - T_min) * (math.exp(float(i) / float(n_replicas-1)) - 1.0) / (math.e - 1.0) for i in range(n_replicas)]
 thermodynamic_states = [states.ThermodynamicState(system=testsystem.system, temperature=T) for T in temperatures]
+
+_logger = logging.getLogger()
+_logger.setLevel(logging.DEBUG)
 
 move = mcmc.LangevinSplittingDynamicsMove(timestep=4.0*unit.femtoseconds, n_steps=250, collision_rate=5.0 / unit.picosecond, reassign_velocities=False, n_restart_attempts=20, constraint_tolerance=1e-06)
 simulation = ReplicaExchangeSampler(mcmc_moves=move, number_of_iterations=5000, replica_mixing_scheme='swap-neighbors', online_analysis_interval=10)
