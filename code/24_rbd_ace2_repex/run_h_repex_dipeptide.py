90534bddec741e88262c0f6f7cb1c8c7647aa5ee
Ivy Zhang <ivy.zhang@wustl.edu>
2021-04-19 18:27:57 -0400

    move debugger

--- code/24_rbd_ace2_repex/run_h_repex_dipeptide.py
+++ code/24_rbd_ace2_repex/run_h_repex_dipeptide.py
@@ -13,11 +13,11 @@ T_max = 600.0 * unit.kelvin  # Maximum temperature.
 temperatures = [T_min + (T_max - T_min) * (math.exp(float(i) / float(n_replicas-1)) - 1.0) / (math.e - 1.0) for i in range(n_replicas)]
 thermodynamic_states = [states.ThermodynamicState(system=testsystem.system, temperature=T) for T in temperatures]
 
-_logger = logging.getLogger()
-_logger.setLevel(logging.DEBUG)
-
 move = mcmc.LangevinSplittingDynamicsMove(timestep=4.0*unit.femtoseconds, n_steps=250, collision_rate=5.0 / unit.picosecond, reassign_velocities=False, n_restart_attempts=20, constraint_tolerance=1e-06)
 simulation = ReplicaExchangeSampler(mcmc_moves=move, number_of_iterations=5000, replica_mixing_scheme='swap-neighbors', online_analysis_interval=10)
+
+_logger = logging.getLogger()
+_logger.setLevel(logging.DEBUG)
 
 storage_path = '/data/chodera/zhangi/perses_benchmark/repex/31/0/3/alanine_dipeptide.nc'
 reporter = multistate.MultiStateReporter(storage_path, checkpoint_interval=10)
