0056c7a8ccb326c65cd740433b5babb8d4f54499
Ivy Zhang <ivy.zhang@wustl.edu>
2021-04-28 20:43:21 -0400

    update protocol

--- code/23_rbd_ace2_neq/run_neq_distrib_seed_general.py
+++ code/23_rbd_ace2_neq/run_neq_distrib_seed_general.py
@@ -26,19 +26,30 @@ args = parser.parse_args()
 
 # Define lambda functions
 x = 'lambda'
+# DEFAULT_ALCHEMICAL_FUNCTIONS = {
+#                              'lambda_sterics_core': x,
+#                              'lambda_electrostatics_core': x,
+#                              'lambda_sterics_insert': f"select(step({x} - 0.5), 1.0, 2.0 * {x})",
+#                              'lambda_sterics_delete': f"select(step({x} - 0.5), 2.0 * ({x} - 0.5), 0.0)",
+#                              'lambda_electrostatics_insert': f"select(step({x} - 0.5), 2.0 * ({x} - 0.5), 0.0)",
+#                              'lambda_electrostatics_delete': f"select(step({x} - 0.5), 1.0, 2.0 * {x})",
+#                              'lambda_bonds': x,
+#                              'lambda_angles': x,
+#                              'lambda_torsions': x}
+
+inflection1, inflection2, inflection3, inflection4 = 0.2, 0.4, 0.6, 0.8
 DEFAULT_ALCHEMICAL_FUNCTIONS = {
                              'lambda_sterics_core': x,
                              'lambda_electrostatics_core': x,
-                             'lambda_sterics_insert': f"select(step({x} - 0.5), 1.0, 2.0 * {x})",
-                             'lambda_sterics_delete': f"select(step({x} - 0.5), 2.0 * ({x} - 0.5), 0.0)",
-                             'lambda_electrostatics_insert': f"select(step({x} - 0.5), 2.0 * ({x} - 0.5), 0.0)",
-                             'lambda_electrostatics_delete': f"select(step({x} - 0.5), 1.0, 2.0 * {x})",
+                             'lambda_sterics_insert': f"select(step({x} - {inflection3}), select(step({x}-{inflection4}), 1, (1/{inflection1})*({x}-{inflection3}), 0.0)",
+                             'lambda_sterics_delete': f"select(step({x} - {inflection1}), select(step({x} - {inflection2}), 1, (1/{inflection1})*({x}-{inflection1})), 0.0)",
+                             'lambda_electrostatics_insert': f"select(step({x} - {inflection4}), (1/{inflection1}) * ({x} - {inflection4}), 0.0)",
+                             'lambda_electrostatics_delete': f"select(step({x} - {inflection1}), 1.0, (1/{inflection1})*{x})",
                              'lambda_bonds': x,
                              'lambda_angles': x,
                              'lambda_torsions': x}
 
 # Define simulation parameters
-# nsteps_eq = 25000 # 100 ps 
 nsteps_eq = 10
 nsteps_neq = int(args.length*250000) # 1 ns
 neq_splitting='V R H O R V'
@@ -58,6 +69,11 @@ with open(os.path.join(args.dir, f"{i}_{args.phase}_{args.old_aa_name}_{cache_le
 positions = subset_pos[args.sim_number]
 system = htf.hybrid_system
 
+# Read in lambda = 0 cache box vectors
+with open(os.path.join(args.dir, f"{i}_{args.phase}_{args.old_aa_name}_{cache_length}ns_box_vectors.npy"), 'rb') as f:
+    subset_box_vectors = np.load(f)
+box_vectors = subset_box_vectors[args.sim_number][0]
+
 # Set up integrator
 integrator = PeriodicNonequilibriumIntegrator(DEFAULT_ALCHEMICAL_FUNCTIONS, nsteps_eq, nsteps_neq, neq_splitting, timestep=timestep, temperature=temperature)
 
@@ -68,97 +84,74 @@ if platform_name in ['CUDA', 'OpenCL']:
 if platform_name in ['CUDA']:
     platform.setPropertyDefaultValue('DeterministicForces', 'true')
 context = openmm.Context(system, integrator, platform)
-context.setPeriodicBoxVectors(*system.getDefaultPeriodicBoxVectors())
+context.setPeriodicBoxVectors(*box_vectors)
 context.setPositions(positions)
 context.setVelocitiesToTemperature(temperature)
 
-# Minimize
-openmm.LocalEnergyMinimizer.minimize(context)
-
 # Run eq forward (0 -> 1)
-forward_eq_old, forward_eq_new = list(), list()
-for fwd_step in range(nsteps_eq):
-    initial_time = time.time()
-    integrator.step(1)
-    elapsed_time = (time.time() - initial_time) * unit.seconds
-    # if fwd_step % 2500 == 0:
-    #     _logger.info(f'forward EQ step: {fwd_step}, took: {elapsed_time / unit.seconds} seconds')
-    #     pos = context.getState(getPositions=True, enforcePeriodicBox=False).getPositions(asNumpy=True)
-    #     old_pos = np.asarray(htf.old_positions(pos))
-    #     new_pos = np.asarray(htf.new_positions(pos))
-    #     forward_eq_old.append(old_pos)
-    #     forward_eq_new.append(new_pos)
+integrator.step(nsteps_eq)
 
 # Run neq forward (0 -> 1)
 forward_works_master = list()
 forward_neq_old, forward_neq_new = list(), list()
 forward_works = [integrator.get_protocol_work(dimensionless=True)]
-for fwd_step in range(nsteps_neq):
-    initial_time = time.time()
-    integrator.step(1)
-    elapsed_time = (time.time() - initial_time) * unit.seconds
+for fwd_step in range(nsteps_neq / 2500):
+    integrator.step(2500)
+    _logger.info(f"Forward neq: {fwd_step*2500} completed")
+
     forward_works.append(integrator.get_protocol_work(dimensionless=True))
-    if fwd_step % 2500 == 0:
-        _logger.info(f'forward NEQ step: {fwd_step}, took: {elapsed_time / unit.seconds} seconds')
-        pos = context.getState(getPositions=True, enforcePeriodicBox=False).getPositions(asNumpy=True)
-        old_pos = np.asarray(htf.old_positions(pos))
-        old_traj = md.Trajectory(old_pos, md.Topology.from_openmm(htf._topology_proposal.old_topology))
-        old_pos_solute = old_traj.atom_slice(old_traj.top.select("not water")).xyz[0]
-        
-        new_pos = np.asarray(htf.new_positions(pos))
-        new_traj = md.Trajectory(new_pos, md.Topology.from_openmm(htf._topology_proposal.new_topology))
-        new_pos_solute = new_traj.atom_slice(new_traj.top.select("not water")).xyz[0]
-        
-        forward_neq_old.append(old_pos_solute)
-        forward_neq_new.append(new_pos_solute)
+    
+    pos = context.getState(getPositions=True, enforcePeriodicBox=False).getPositions(asNumpy=True)
+    old_pos = np.asarray(htf.old_positions(pos))
+    old_traj = md.Trajectory(old_pos, md.Topology.from_openmm(htf._topology_proposal.old_topology))
+    old_pos_solute = old_traj.atom_slice(old_traj.top.select("not water")).xyz[0]
+    
+    new_pos = np.asarray(htf.new_positions(pos))
+    new_traj = md.Trajectory(new_pos, md.Topology.from_openmm(htf._topology_proposal.new_topology))
+    new_pos_solute = new_traj.atom_slice(new_traj.top.select("not water")).xyz[0]
+    
+    forward_neq_old.append(old_pos_solute)
+    forward_neq_new.append(new_pos_solute)
 forward_works_master.append(forward_works)
 
 # Read in lambda = 1 cache, if necessary
 with open(os.path.join(args.dir, f"{i}_{args.phase}_{args.new_aa_name}_{cache_length}ns_snapshots.npy"), 'rb') as f:
     subset_pos = np.load(f)
 positions = subset_pos[args.sim_number]
+
+# Read in lambda = 1 cache box vectors
+with open(os.path.join(args.dir, f"{i}_{args.phase}_{args.new_aa_name}_{cache_length}ns_box_vectors.npy"), 'rb') as f:
+    subset_box_vectors = np.load(f)
+box_vectors = subset_box_vectors[args.sim_number][0]
+
+context.setPeriodicBoxVectors(*box_vectors)
 context.setPositions(positions)
 context.setVelocitiesToTemperature(temperature)
 
-# Minimize
-openmm.LocalEnergyMinimizer.minimize(context)
-
 # Run eq reverse (1 -> 0)
-reverse_eq_old, reverse_eq_new = list(), list()
-for rev_step in range(nsteps_eq):
-    initial_time = time.time()
-    integrator.step(1)
-    elapsed_time = (time.time() - initial_time) * unit.seconds
-    # if rev_step % 2500 == 0:
-    #     _logger.info(f'reverse EQ step: {rev_step}, took: {elapsed_time / unit.seconds} seconds')
-    #     pos = context.getState(getPositions=True, enforcePeriodicBox=False).getPositions(asNumpy=True)
-    #     old_pos = np.asarray(htf.old_positions(pos))
-    #     new_pos = np.asarray(htf.new_positions(pos))
-    #     reverse_eq_old.append(old_pos)
-    #     reverse_eq_new.append(new_pos)
+integrator.step(nsteps_eq)
 
 # Run neq reverse (1 -> 0)
 reverse_works_master = list()
 reverse_neq_old, reverse_neq_new = list(), list()
 reverse_works = [integrator.get_protocol_work(dimensionless=True)]
-for rev_step in range(nsteps_neq):
-    initial_time = time.time()
-    integrator.step(1)
-    elapsed_time = (time.time() - initial_time) * unit.seconds
+for rev_step in range(nsteps_neq / 2500):
+    integrator.step(2500)
+    _logger.info(f"Reverse neq: {rev_step*2500} completed")
+    
     reverse_works.append(integrator.get_protocol_work(dimensionless=True))
-    if rev_step % 2500 == 0:
-        _logger.info(f'reverse NEQ step: {rev_step}, took: {elapsed_time / unit.seconds} seconds')
-        pos = context.getState(getPositions=True, enforcePeriodicBox=False).getPositions(asNumpy=True)
-        old_pos = np.asarray(htf.old_positions(pos))
-        old_traj = md.Trajectory(old_pos, md.Topology.from_openmm(htf._topology_proposal.old_topology))
-        old_pos_solute = old_traj.atom_slice(old_traj.top.select("not water")).xyz[0]
-        
-        new_pos = np.asarray(htf.new_positions(pos))
-        new_traj = md.Trajectory(new_pos, md.Topology.from_openmm(htf._topology_proposal.new_topology))
-        new_pos_solute = new_traj.atom_slice(new_traj.top.select("not water")).xyz[0]
+    
+    pos = context.getState(getPositions=True, enforcePeriodicBox=False).getPositions(asNumpy=True)
+    old_pos = np.asarray(htf.old_positions(pos))
+    old_traj = md.Trajectory(old_pos, md.Topology.from_openmm(htf._topology_proposal.old_topology))
+    old_pos_solute = old_traj.atom_slice(old_traj.top.select("not water")).xyz[0]
+    
+    new_pos = np.asarray(htf.new_positions(pos))
+    new_traj = md.Trajectory(new_pos, md.Topology.from_openmm(htf._topology_proposal.new_topology))
+    new_pos_solute = new_traj.atom_slice(new_traj.top.select("not water")).xyz[0]
 
-        reverse_neq_old.append(old_pos_solute)
-        reverse_neq_new.append(new_pos_solute)
+    reverse_neq_old.append(old_pos_solute)
+    reverse_neq_new.append(new_pos_solute)
 reverse_works_master.append(reverse_works)
 
 # Save works
@@ -166,16 +159,6 @@ with open(os.path.join(args.dir, f"{i}_{args.phase}_{args.sim_number}_forward.np
     np.save(f, forward_works_master)
 with open(os.path.join(args.dir, f"{i}_{args.phase}_{args.sim_number}_reverse.npy"), 'wb') as f:
     np.save(f, reverse_works_master)
-
-# Save trajs
-# with open(os.path.join(args.dir, f"{i}_{args.phase}_{args.sim_number}_forward_eq_old.npy"), 'wb') as f:
-#     np.save(f, forward_eq_old)
-# with open(os.path.join(args.dir, f"{i}_{args.phase}_{args.sim_number}_forward_eq_new.npy"), 'wb') as f:
-#     np.save(f, forward_eq_new)
-# with open(os.path.join(args.dir, f"{i}_{args.phase}_{args.sim_number}_reverse_eq_old.npy"), 'wb') as f:
-#     np.save(f, reverse_eq_old)
-# with open(os.path.join(args.dir, f"{i}_{args.phase}_{args.sim_number}_reverse_eq_new.npy"), 'wb') as f:
-#     np.save(f, reverse_eq_new)
 
 with open(os.path.join(args.dir, f"{i}_{args.phase}_{args.sim_number}_forward_neq_old.npy"), 'wb') as f:
     np.save(f, forward_neq_old)
