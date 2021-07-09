433b98db9ef4a509af26dc58889fe6a84fd5d9d1
Ivy Zhang <ivy.zhang@wustl.edu>
2021-07-09 16:33:22 -0400

    update flags

--- code/26_rxn_field/test_bool.py
+++ code/26_rxn_field/test_bool.py
@@ -16,15 +16,16 @@ _logger.setLevel(logging.INFO)
 
 # Read args
 parser = argparse.ArgumentParser(description='run perses protein mutation on capped amino acid')
-parser.add_argument('is_rxn_field', type=bool, help='whether to use rxn field protocol')
+parser.add_argument('--is_rxn_field', dest='is_rxn_field', action='store_true', help='whether to use rxn field protocol')
+parser.add_argument('--not_rxn_field', dest='is_rxn_field', action='store_false', help='whether to use rxn field protocol')
 args = parser.parse_args()
 
 # Define lambda functions
 _logger.info(f"Defining lambda fn")
 
-print(args.is_rxn_field)
+print(is_rxn_field)
 x = 'lambda'
-if not args.is_rxn_field:
+if not is_rxn_field:
     print("pme")
 
 else:
