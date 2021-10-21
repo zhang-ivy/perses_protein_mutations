92ac80f2cfb656d6d3a8eb5f0af71ca22535539e
zhang-ivy <ivy.zhang@wustl.edu>
2021-10-21 15:28:36 -0400

    fix indentation

--- code/32_rbd_ace2_direct_repex/prep_repex_jobs.py
+++ code/32_rbd_ace2_direct_repex/prep_repex_jobs.py
@@ -12,39 +12,39 @@ n_chunks = 10
 name = "32.0"
 
 for i in range(n_chunks):
-	print(f"prepping dir {i}")
+    print(f"prepping dir {i}")
 
-        lambda_start = i * 0.1
-        lambda_end = lambda_start + 0.1
+    lambda_start = i * 0.1
+    lambda_end = lambda_start + 0.1
 
-	for i, phase in enumerate(phases):
+    for i, phase in enumerate(phases):
 
-		# Edit template bash file
-		with open(template_file, "r") as f:
-			lines_out = f.readlines()
+        # Edit template bash file
+        with open(template_file, "r") as f:
+            lines_out = f.readlines()
 
-		lines_new = []
-		for line in lines_out:
-			if "#BSUB -W" in line:
-				line = line[:9] + str(wall_time[i]) + line[11:]
-			elif "#BSUB -o" in line:
-				line = line[:13] + "{phase}.out\n"
-			elif "#BSUB -eo" in line:
-				line = line[:13] + "{phase}.stderr\n"
-			elif "#BSUB -n" in line:
-				line = line[:26] + str(memory[i]) + line[27:] 
-			elif "#BSUB -J" in line:
-				line = line[:13] + name + f'.{i}"\n'
-                        elif "new=" in line:
-                                line = f"new={i}\n"
-			elif "phase=" in line:
-				line = f"phase={phase}\n"
-                        elif "lambda_start=" in line:
-                                line = f"lambda_start={lambda_start}\n"
-                        elif "lambda_end=" in line:
-                                line = f"lambda_end={lambda_end}\n"
-			lines_new.append(line)
+        lines_new = []
+        for line in lines_out:
+            if "#BSUB -W" in line:
+                line = line[:9] + str(wall_time[i]) + line[11:]
+            elif "#BSUB -o" in line:
+                line = line[:13] + "{phase}.out\n"
+            elif "#BSUB -eo" in line:
+                line = line[:13] + "{phase}.stderr\n"
+            elif "#BSUB -n" in line:
+                line = line[:26] + str(memory[i]) + line[27:] 
+            elif "#BSUB -J" in line:
+                line = line[:13] + name + f'.{i}"\n'
+                    elif "new=" in line:
+                            line = f"new={i}\n"
+            elif "phase=" in line:
+                line = f"phase={phase}\n"
+            elif "lambda_start=" in line:
+                            line = f"lambda_start={lambda_start}\n"
+                    elif "lambda_end=" in line:
+                            line = f"lambda_end={lambda_end}\n"
+            lines_new.append(line)
 
 
-		with open(os.path.join(out_dir, i, f"run_repex_parallel_{phase}_scatter.sh"), "w") as f:
-			f.writelines(lines_new)
+        with open(os.path.join(out_dir, i, f"run_repex_parallel_{phase}_scatter.sh"), "w") as f:
+            f.writelines(lines_new)
