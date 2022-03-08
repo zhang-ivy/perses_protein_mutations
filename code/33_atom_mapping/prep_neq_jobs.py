ec4d5ece9f6f8ab376a142f86450d5c734fada87
zhang-ivy <ivy.zhang@wustl.edu>
2022-03-08 11:36:08 -0500

    fix spacing

--- code/33_atom_mapping/prep_neq_jobs.py
+++ code/33_atom_mapping/prep_neq_jobs.py
@@ -19,45 +19,44 @@ chain_B = 2
 phases = ['complex', 'apo']
 
 for k, v in dictionary.items():
-	print(f"prepping dir {k}")
-	new = k
-	old_aa, new_aa, resid = v
+    print(f"prepping dir {k}")
+    new = k
+    old_aa, new_aa, resid = v
 
-	for i, phase in enumerate(phases):
+    for i, phase in enumerate(phases):
 
-		# Edit template bash file
-		with open(template_file, "r") as f:
-			lines_out = f.readlines()
+        # Edit template bash file
+        with open(template_file, "r") as f:
+            lines_out = f.readlines()
 
-		lines_new = []
-		for line in lines_out:
-			if "#BSUB -W" in line:
-				line = line[:9] + str(wall_time[i]) + line[10:]
-			elif "#BSUB -o" in line:
-				line = line[:13] + str(new) + f".{phase}.%I.out\n"
-			elif "#BSUB -eo" in line:
-				line = line[:13] + str(new) + f".{phase}.%I.stderr\n"
-			elif "#BSUB -n" in line:
-				line = line[:26] + str(memory[i]) + line[27:] 
-			elif "#BSUB -J" in line:
-				line = line[:13] + str(new) + f'.{phase}.[1-100]"\n'
-			elif "#BSUB -w" in line:
-				job = job_apo if phase == 'apo' else job_complex
-				line = line[:9] + f'"done({job})"\n'
-			elif "new=" in line:
-				line = f"new={new}\n"
-			elif "phase=" in line:
-				line = f"phase={phase}\n"
-			elif "eq_length=" in line:
-				line = f"eq_length={eq_length}\n"
-                        elif "neq_length=" in line:
-                                line = f"neq_length={neq_length}\n"
-                        elif "chain_A=" in line:
-                                line = f"chain_A={chain_A}\n"
-                        elif "chain_B=" in line:
-                                line = f"chain_B={chain_B}\n"
-			lines_new.append(line)
+        lines_new = []
+        for line in lines_out:
+            if "#BSUB -W" in line:
+                line = line[:9] + str(wall_time[i]) + line[10:]
+            elif "#BSUB -o" in line:
+                line = line[:13] + str(new) + f".{phase}.%I.out\n"
+            elif "#BSUB -eo" in line:
+                line = line[:13] + str(new) + f".{phase}.%I.stderr\n"
+            elif "#BSUB -n" in line:
+                line = line[:26] + str(memory[i]) + line[27:] 
+            elif "#BSUB -J" in line:
+                line = line[:13] + str(new) + f'.{phase}.[1-100]"\n'
+            elif "#BSUB -w" in line:
+                job = job_apo if phase == 'apo' else job_complex
+                line = line[:9] + f'"done({job})"\n'
+            elif "new=" in line:
+                line = f"new={new}\n"
+            elif "phase=" in line:
+                line = f"phase={phase}\n"
+            elif "eq_length=" in line:
+                line = f"eq_length={eq_length}\n"
+            elif "neq_length=" in line:
+                line = f"neq_length={neq_length}\n"
+            elif "chain_A=" in line:
+                line = f"chain_A={chain_A}\n"
+            elif "chain_B=" in line:
+                line = f"chain_B={chain_B}\n"
+            lines_new.append(line)
 
-
-		with open(os.path.join(out_dir, str(new), f"run_neq_{phase}.sh"), "w") as f:
-			f.writelines(lines_new)
+        with open(os.path.join(out_dir, str(new), f"run_neq_{phase}.sh"), "w") as f:
+                f.writelines(lines_new)
